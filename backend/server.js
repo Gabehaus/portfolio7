e23cdde337808b382332fd35a37edc4bf8d2b2f9
Gabehaus <Gabehaus@gmail.com>
2021-05-12 23:10:26 -0700

    first commit of full-stack template minus redux

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express"
+import dotenv from "dotenv"
+import colors from "colors"
+import morgan from "morgan"
+
+import cors from "cors"
+import test from "./routes/test.js"
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"))
+}
+
+app.use(express.json())
+
+app.use("/api/test", test)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`.yellow.bold))
