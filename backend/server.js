a9810a90d8be53294dd31c492eb9ff84d1be589b
Gabehaus <Gabehaus@gmail.com>
2021-07-20 00:49:35 -0700

    fixed server.js

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,21 @@ app.use(express.json())
 app.use("/api/test", test)
 app.use("/api/mail", mail)
 
+const __dirname = path.resolve()
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")))
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  )
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....")
+  })
+}
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`.yellow.bold))
